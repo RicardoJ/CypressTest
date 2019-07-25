06fd6cef2d6f1a14cf516fc5d7ec6d38d303c359
RicardoJ <rjdiazzh@gmail.com>
2019-07-25 17:30:59 -0500

    agregando funcionalidad a la automatizacion

--- cypress/integration/examples/TRANSALEGOOGLE.spec.js
+++ cypress/integration/examples/TRANSALEGOOGLE.spec.js
@@ -2,4 +2,9 @@ describe('My first cypress test', () => {
   it('Navigate to google transale', () => {
     cy.visit('https://translate.google.com/?source=gtx');
   });
+  it('translate word we erase and write again ', () => {
+    cy.get('#source').type('Hola');
+    cy.get('.clear > .jfk-button-img').click();
+    cy.get('#source').type('Hola world');
+  });
 });
